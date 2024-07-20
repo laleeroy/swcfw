2006ceb1ebefe5c1fc9e1e1a0eb07c97e715a8ab
laleeroy <gabwielcastwo@gmail.com>
2024-07-20 22:19:33 +0800

    exclude more
    Signed-off-by: laleeroy <gabwielcastwo@gmail.com>

--- archive.sh
+++ archive.sh
@@ -6,7 +6,7 @@ REPO_TAG_NAME=$1
 # Define the output zip file name
 ZIP_FILE="8BP-$REPO_TAG_NAME.zip"
 
-# Find all files in the current directory except .sh files and the .git folder, and create a zip file
-zip -r "$ZIP_FILE" . -x "*.sh" -x ".git/*"
+# Find all files in the current directory except .sh files and the git related directories, and create a zip file
+zip -r "$ZIP_FILE" . -x "*.sh" -x ".git/*" -x ".github/*"
 
 echo "Files have been zipped into $ZIP_FILE, excluding all .sh files and the .git folder."
